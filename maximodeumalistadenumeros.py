9b6cb34f0eca9a28242a1562eada94a372eb8029
Your Name <dayannagouveia41@gmail.com>
2019-02-21 07:48:45 -0300

    aulapython

---
+++ maximodeumalistadenumeros.py
@@ -0,0 +1,28 @@
+def main():
+    ''' teste da funcao maximo '''
+    minha_lista = [12,34,78,90,5]
+    maior_numero, indice = max_lista(minha_lista)
+    print("o maior elemento é:", maior_numero)
+    print("0 indice é =", indice) 
+
+
+
+
+def max_lista(lista):
+    '''(list) -> int, int
+    recebe uma lista de inteiros e retorna o
+    valor maximo da lista e o indice onde ele ocorre'''
+
+    maior = -1
+
+    for n in lista:
+        if n > maior
+        print(n)
+        
+
+
+
+    return maior, 0
+
+main()
+  
